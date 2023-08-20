2b595110451a53b4ce73a952f419f86e82f14dc5
hyungkwonko <hyungkwonko@gmail.com>
2023-08-20 12:19:44 +0900

    update

--- preprocess.py
+++ preprocess.py
@@ -35,20 +35,20 @@ vl_files = np.sort(glob(vl_files))
 data_files = np.sort(glob(data_dir))
 
 for i in range(len(vl_files)):
-	with open(vl_files[i], "r") as file:
-		vl = json.load(file)
-	keys, _ = get_fields_and_types(vl)
+    with open(vl_files[i], "r") as file:
+        vl = json.load(file)
+    keys, _ = get_fields_and_types(vl)
+    keys = list(set(keys))
+    print(f"i: {i}, data_files[i]: {data_files[i]}, keys: {keys}")
 
-	print(f"i: {i}, data_files[i]: {data_files[i]}, keys: {keys}")
-
-	if data_files[i].endswith('.json'):
-		df = pd.read_json(data_files[i])
-		new_keys = [key for key in keys if key in df.columns]
-		subset = df[new_keys]
-		subset.to_csv(data_files[i].replace('.json', '.csv').replace('data_raw', 'data_process'), index=False)
-	elif data_files[i].endswith('.csv'):
-		df = pd.read_csv(data_files[i])
-		new_keys = [key for key in keys if key in df.columns]
-		subset = df[new_keys]
-		subset.to_csv(data_files[i].replace('data_raw', 'data_process'), index=False)
+    if data_files[i].endswith('.json'):
+        df = pd.read_json(data_files[i])
+        new_keys = [key for key in keys if key in df.columns]
+        subset = df[new_keys]
+        subset.to_csv(data_files[i].replace('.json', '.csv').replace('data_raw', 'data_process'), index=False)
+    elif data_files[i].endswith('.csv'):
+        df = pd.read_csv(data_files[i])
+        new_keys = [key for key in keys if key in df.columns]
+        subset = df[new_keys]
+        subset.to_csv(data_files[i].replace('data_raw', 'data_process'), index=False)
 
